a4ffafd22d1f2b017ca4e0a8e03df2ed8adbbac3
andre aguiar <aguiar1001@hotmail.com>
2020-07-23 11:39:56 -0300

    arrumando o front

--- app/controllers/freights_controller.rb
+++ app/controllers/freights_controller.rb
@@ -3,4 +3,5 @@ class FreightsController < ApplicationController
         power_generator = PowerGenerator.find(params[:id])
         params[:address] = HTTParty.get("http://apps.widenet.com.br/busca-cep/api/cep/#{params[:zipcode]}.json")
     end
-end
+
+ end
--- app/models/freight.rb
+++ app/models/freight.rb
@@ -1,19 +1,22 @@
+require 'correios-frete'
+
+
 class Freight < ApplicationRecord
   validates :state, :weight_min, :weight_max, :cost, presence: true
 
 # RETIRADO DE https://github.com/prodis/correios-frete
 # endereço portal solar R. Barão do Triunfo, 612 - Brooklin, São Paulo - SP, 04602-002
-def calculate_ship
-  frete = Correios::Frete::Calculador.new :cep_origem => "04602-002",
-  :cep_destino => params[:zipcode]
-  
-  servicos = frete.calcular :sedex, :pac
-  @pac = servicos[:pac].valor
-  @sedex = servicos[:sedex].valor
-  render 'checkout'
-end
 
 
-
+  def calculate_ship(cep_destino)
+    frete = Correios::Frete::Calculador.new :cep_origem => "04602-002", :cep_destino => params[:zipcode]
+    
+    servicos = frete.calcular :sedex, :pac
+    @pac = servicos[:pac].valor
+    @sedex = servicos[:sedex].valor
+    sedex = frete.calcular :sedex
+    pac = frete.calcular :pac
+    render 'checkout'
+  end
 
 end
